8cef3d5363e4da2b47bbf50063fc45b56f0b665b
Rohan <rohanmaheshrao10@gmail.com>
2022-11-28 13:59:16 +0530

    Final changes added

--- Speckle_Noiser.m
+++ Speckle_Noiser.m
@@ -2,9 +2,9 @@
 % Creating a model to remove speckling noise in a SAR image, using a
 % trained CNN
 
-Noisy_set  = "C:\Users\Rohan Mahesh Rao\Desktop\DIP_project\Noisy_Dataset\";
-Clean_set =  "C:\Users\Rohan Mahesh Rao\Desktop\DIP_project\Dataset\";
-Storage =dir(fullfile("C:","Users","Rohan Mahesh Rao","Desktop","DIP_project","Dataset","*.png"));
+Noisy_set  = "C:\Users\Rohan Mahesh Rao\Desktop\DIP_project\Testset\speckled\";
+Clean_set =  "C:\Users\Rohan Mahesh Rao\Desktop\DIP_project\Testset\clean\";
+Storage =dir(fullfile("C:","Users","Rohan Mahesh Rao","Desktop","DIP_project","Testset","clean","*.png"));
 fprintf("No.of images in the clean set: %d",numel(Storage));
 
 for k = 1: numel(Storage)
@@ -12,12 +12,14 @@ for k = 1: numel(Storage)
     Input = imread(Files);
     Storage(k).data = Input;
 end
+
 for j = 1:length(Storage)
     ImageData = Storage(j).data;
     %apply noise
-    random_parameter = 0.01 + rand*(1.0 - 0.001); 
+    random_parameter = 0.01 + rand*(1.0 - 0.001);
+    disp(random_parameter);
     ApplyNoise = imnoise(ImageData,'speckle',random_parameter);
     %save images in the right folder
-    NoiseLocation = fullfile(Noisy_set,sprintf('noise%d.png',j));
+    NoiseLocation = fullfile(Noisy_set,sprintf('speckled%d.png',j));
     imwrite(ApplyNoise,NoiseLocation);
 end 
